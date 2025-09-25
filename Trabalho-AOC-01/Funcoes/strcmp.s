23edc99cb2b98bd32e2f88da6bedc48a1dec277b
Pedro Pignata <pedropignata2@gmail.com>
2025-09-25 18:49:04 -0300

    strcmp: é uma função que compara duas strings subtraindo e se las forem iguais continnua o loop, mas se forem diferentes o rpograma encerra mandando o valor de a(0)

---
+++ Trabalho-AOC-01/Funcoes/strcmp.s
@@ -0,0 +1,32 @@
+.global strcmp
+.type strcmp, @function
+
+strcmp: 
+    # a0 = ponteiro para s1
+    # a1 = ponteiro para s2
+    # t0 = caractere de s1
+    # t1 = caractere de s2
+
+loop: 
+    # Careegamos um byte de cada string
+    lbu t0, 0(a0)
+    lbu t1, 0(a1)
+    # Comparamos os caracteres por subtração
+    sub a0, t0, t1
+    # Se a difrença for zero eles  são iguais
+    # Verificação se chugou no fim da string
+    beq a0, x0, next
+    
+    ret
+
+next:
+    # Se t0 for nulo chegamos no fim da string dele
+    beq t0, x0, end
+    # Se não for avança um caractere
+    addi a0, a0, 1
+    addi a1, a1, 1
+    j loop
+
+end: 
+    # Retorna para a0(0)
+    ret
