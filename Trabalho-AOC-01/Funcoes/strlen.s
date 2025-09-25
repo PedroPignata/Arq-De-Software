a323e8dc46e5a279a910b24da874c54fbee8254d
Pedro Pignata <pedropignata2@gmail.com>
2025-09-25 18:49:27 -0300

    Arrumando os comentarios

--- Trabalho-AOC-01/Funcoes/strlen.s
+++ Trabalho-AOC-01/Funcoes/strlen.s
@@ -2,22 +2,22 @@
 .type strlen, @function
 
 strlen:
-    # Inicializa o contador de caracteres (len) em a1.
+    # Inicializa o contador de caracteres (len) em a1
     li a1, 0
 
 loop:
-    # Carrega o byte do endereço apontado por a0 para o registrador a2.
+    # Carrega o byte do endereço apontado por a0 para o registrador a2
     lbu a2, 0(a0)
-    # Verifica se o caractere carregado (em a2) é o caractere nulo '\0' (0).
+    # Verifica se o caractere carregado (em a2) é o caractere nulo '\0' (0)
     beq a2, x0, end
-    # Se não for o caractere nulo, incrementa o ponteiro da string.
+    # Se não for o caractere nulo, incrementa o ponteiro da string
     addi a0, a0, 1
-    # Incrementa o contador de caracteres.
+    # Incrementa o contador de caracteres
     addi a1, a1, 1
-    # Volta para o início do loop para processar o próximo caractere.
+    # Volta para o início do loop para processar o próximo caractere
     j loop
 
 end:
-    # Movemos o resultado para a0, que é o registrador de retorno padrão de uma função.
+    # Movemos o resultado para a0, que é o registrador de retorno padrão de uma função
     mv a0, a1
     ret
