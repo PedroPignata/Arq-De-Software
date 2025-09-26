b952c5842dacea8c420c2cdf509380bd5d4a24b4
Pedro Pignata <pedropignata2@gmail.com>
2025-09-26 17:59:55 -0300

    strncmp é uma função que compara os primeiros n caracteres (o terceiro argumento).

---
+++ Trabalho-AOC-01/Funcoes/strncmp.s
@@ -0,0 +1,39 @@
+.global strncmp
+.type strncmp, @function
+
+strncmp:
+
+    # a0 = ponteiro para s1
+    # a1 = ponteiro para s2
+    # a2 = número máximo de caracteres a comparar (n)
+
+loop:
+    # Checar se n(a2) é 0, se for para e retorna a 0
+    beq a2, x0, endn
+    # Carrega um byte de cada string
+    lbu t0, 0(a0)
+    lbu t1, 0(a1)
+    # Decrementa n
+    addi a2, a2, -1
+    # Compara os caracteres por subtração, para ver se é o fim
+    sub a3, t0, t1
+    # Se for diferente de zero retorna a diferença
+    bne a3, x0, endsub
+    # Se for zero são iguais, ai checa se t0 é '\0'(O fim da string)
+    beq, t0, x0, endn
+    #avança os ponteiros
+    addi a0, a0, 1
+    addi a1, a1, 1
+    
+    j loop
+
+    ret
+
+endsub:
+    # Retorna a diferença
+    mv a0, a3
+    j loop
+
+endn:
+    mv a0, x0
+    ret
